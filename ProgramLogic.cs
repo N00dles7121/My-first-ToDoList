2acdf9e2ffcef25791dac0cfb669c88756b249af
Albert Kargaev <n00dles7121@gmail.com>
2024-12-06 20:13:20 -0500

    added comments to ProgramLogic.cs minor ProgramLogic.cs code optimisation

--- ProgramLogic.cs
+++ ProgramLogic.cs
@@ -9,6 +9,7 @@ namespace ToDoList
     {
         public static void ShowTaskList(List<string> taskList)
         {
+            //  Checks if list is empty
             if (taskList.Count == 0)
             {
                 System.Console.WriteLine("Your task list is empty.");
@@ -18,15 +19,17 @@ namespace ToDoList
             System.Console.WriteLine("----------------------");
             System.Console.WriteLine("Hese's your task list:");
 
+            // Displays all the tasks
             for (int i = 0; i < taskList.Count; i++)
             {
-                    Console.WriteLine(i + 1 + " - " + taskList[i]);
+                Console.WriteLine(i + 1 + " - " + taskList[i]);
             }
 
             System.Console.WriteLine("----------------------");
         }
 
-        static private bool CheckForDuplicates(List<string> tasks, string task) {
+        static private bool CheckForDuplicates(List<string> tasks, string task)
+        {
             return tasks.Contains(task);
         }
 
@@ -34,13 +37,19 @@ namespace ToDoList
         {
             ShowTaskList(taskList);
 
+            // Request user inputs
             System.Console.WriteLine("Input task number to delete.");
-            if (int.TryParse(Console.ReadLine(), out int taskNumber) && taskList.Count >= taskNumber)
+
+            // Check if user input is valid option
+            if (int.TryParse(Console.ReadLine(), out int taskNumber) && taskNumber > 0 && taskNumber <= taskList.Count)
             {
+
+                // Remove task at specified index and ask for repeated removal
                 taskList.RemoveAt(taskNumber - 1);
                 System.Console.WriteLine($"\nTask #{taskNumber} was succesfully removed.");
                 System.Console.Write("Would you like to add another task (Y/N): ");
 
+                // Call method again and stop it after completion or return to main menu
                 if (Console.ReadLine().ToUpper() == "Y")
                 {
                     RemoveTask(taskList);
@@ -48,10 +57,10 @@ namespace ToDoList
                 }
                 else return;
             }
+
+            // Ask if user wants to input another option or return to main menu
             else
             {
-                do
-                {
                     System.Console.WriteLine("\nWrong input.\n1 - try again.\n0 - main menu.");
                     if (Console.ReadLine() == "1")
                     {
@@ -60,49 +69,40 @@ namespace ToDoList
                     }
                     else
                     {
-                        break;
+                        return;
                     }
-                } while (Console.ReadLine() != "1" || Console.ReadLine() != "0");
             }
         }
 
         public static void AddTask(List<string> taskList)
         {
             string menuNavigation = "";
-            bool isTrying = true;
 
-            do
+            System.Console.WriteLine("Please type in the task:");
+            string taskToAdd = Console.ReadLine();
+            if (taskToAdd == "" || CheckForDuplicates(taskList, taskToAdd))
             {
-                System.Console.WriteLine("Please type in the task:");
-                string taskToAdd = Console.ReadLine();
-                if (taskToAdd == "" || CheckForDuplicates(taskList, taskToAdd))
-                {
-                    System.Console.WriteLine("\nTask can not be empty or this task already exists.\n1 - try again.\n0 - main menu.");
-                    menuNavigation = Console.ReadLine();
-                    if (menuNavigation == "1")
-                    {
-                        AddTask(taskList);
-                        return;
-                    }
-                    else break;
-                }
-
-                taskList.Add(taskToAdd);
-
-                System.Console.WriteLine("\nYour task succesfully added.");
-                System.Console.Write("Would you like to add another task (Y/N): ");
-                menuNavigation = Console.ReadLine().ToUpper();
-                if (menuNavigation == "Y")
+                System.Console.WriteLine("\nTask can not be empty or this task already exists.\n1 - try again.\n0 - main menu.");
+                menuNavigation = Console.ReadLine();
+                if (menuNavigation == "1")
                 {
                     AddTask(taskList);
                     return;
                 }
-                else break;
-            } while (isTrying);
+                else return;
+            }
 
-            Console.Clear();
-            return;
+            taskList.Add(taskToAdd);
+
+            System.Console.WriteLine("\nYour task succesfully added.");
+            System.Console.Write("Would you like to add another task (Y/N): ");
+            menuNavigation = Console.ReadLine().ToUpper();
+            if (menuNavigation == "Y")
+            {
+                AddTask(taskList);
+                return;
+            }
+            else return;
         }
-
     }
 }
--- obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
+++ obj/Debug/net8.0/ToDoList.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9fd8ae361ce6411e3333ac9cfb4d06b693c151e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa52abce6ebb5f4fb92de3336555c0ff7fad6c34")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
